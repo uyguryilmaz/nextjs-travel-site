03e9a5583a49ecb78c691dd5d648db8e0609ab62
Uygur Yılmaz <uygurnex@gmail.com>
2025-08-30 18:26:49 +0300

    add Swiper.js slider and hero background

---
+++ app/(routes)/(home)/_components/Hero/Hero.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { slides } from "@/constans";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Image from "next/image";
+
+const Hero = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <div className=" relative  bg-[url('/slider/bgswipe.jpg')] py-16">
+      <div className="container mx-auto px-4 flex flex-col lg:flex-row items-center">
+        {/** Left */}
+
+        <div className="lg:w-1/2 space-y-6 text-center lg:text-left px-6 transition-all duration-500">
+          <h4 className="text-orange-600 font-bold text-lg">Lets Go Now</h4>
+          <h1 className=" text-3xl md:text-5xl font-semibold text-gray-900">
+            {slides[currentSlide].title}
+          </h1>
+
+          <p className="text-gray-600 text-base md:text-lg">
+            {slides[currentSlide].description}
+          </p>
+
+          <Button className="bg-orange-500 hover:bg-orange-600">
+            {slides[currentSlide].buttonText}
+          </Button>
+        </div>
+
+        {/** Right*/}
+
+        <div className="lg:w-1/2 mt-12 lg:mt-0 w-full">
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 3500,
+              disableOnInteraction: false,
+            }}
+            navigation={true}
+            loop={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="w-full"
+            onSlideChange={(swiper) => setCurrentSlide(swiper.realIndex)}
+          >
+            {slides.map((slide, index) => (
+              <SwiperSlide key={index}>
+                <div className="relative rounded-3xl overflow-hidden">
+                  <Image
+                    src={slide.image}
+                    alt={slide.title}
+                    width={800}
+                    height={600}
+                    className="w-full h-auto object-cover"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,11 +1,13 @@
 import RecentProduct from "@/components/sections/recent/RecentProduct";
-import Image from "next/image";
+import Hero from "./_components/Hero/Hero";
+
 
 
 
 export default function Home() {
   return (
     <div>
+      <Hero/>
       <RecentProduct/>
     </div>
   );
